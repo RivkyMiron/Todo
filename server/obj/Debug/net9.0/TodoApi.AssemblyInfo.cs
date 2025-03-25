a1c8b93e071875f43cf0794cdbd053d0c752c4d2
Nechama_Cohen <nc7813@gmail.com>
2025-03-25 14:21:24 +0200

    change docke21r

--- server/obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b82514804b027fc8918936e6d81fea6fdebd28e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cc84d46ac6cb8b3248f2f0e1214ca835d404220")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
